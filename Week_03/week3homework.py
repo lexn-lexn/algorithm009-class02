96ae2ce6ee0304fc2d513a5f4e6819b77872f9cd
lexn-lexn <liuxpro123@163.com>
2020-06-07 09:20:46 +0800

    week3home

---
+++ Week_03/week3homework.py
@@ -0,0 +1,44 @@
+#问题一  全排列有重复
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        self.res = []
+        check = [0 for i in range(len(nums))]
+        
+        self.backtrack([], nums, check)
+        return self.res
+        
+    def backtrack(self, sol, nums, check):
+        if len(sol) == len(nums):
+            self.res.append(sol)
+            return
+        
+        for i in range(len(nums)):
+            if check[i] == 1:
+                continue
+            if i > 0 and nums[i] == nums[i-1] and check[i-1] == 0:
+                continue
+            check[i] = 1
+            self.backtrack(sol+[nums[i]], nums, check)
+            check[i] = 0
+
+
+# 问题二 全排列 无重复
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) <= 1:  # 递归终止条件
+            return [nums]
+        res = []
+        for idx, num in enumerate(nums):
+            res_nums = nums[:idx] + nums[idx + 1:]  # 确定剩余元素
+            for j in self.permute(res_nums):  
+                res.append([num] + j)
+        return res
